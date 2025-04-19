fdee8bdc3f2833899dd789fee8d2e8d83993ed5d
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-04-19 19:09:49 -0300

    create configuration property

--- src/main/java/com/development/software/world/real/service/impl/FileReaderImpl.java
+++ src/main/java/com/development/software/world/real/service/impl/FileReaderImpl.java
@@ -1,7 +1,8 @@
 package com.development.software.world.real.service.impl;
 
-import jakarta.enterprise.context.ApplicationScoped;
 import com.development.software.world.real.service.FileReader;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,11 +12,11 @@ import java.util.List;
 @ApplicationScoped
 public class FileReaderImpl implements FileReader {
 
-    private static final String RESOURCES = "src/main/resources";
-
+    @ConfigProperty(name = "file.reader.default.path", defaultValue = "src/main/resources")
+    String resources;
 
     @Override
     public List<String> readAllLines(String stringPath) throws IOException {
-        return Files.readAllLines(Path.of(RESOURCES + stringPath));
+        return Files.readAllLines(Path.of(resources + stringPath));
     }
 }
